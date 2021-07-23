6365c5f334671be9a12d859ad05e2fce80a75e01
TtuTtuMan <vldpfh548@gmail.com>
2021-07-23 20:12:46 +0900

    흑백모드추가

--- first_flutter_app/lib/main.dart
+++ first_flutter_app/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:ui';
-import '../todo.dart';
+import 'todo.dart';
 import 'coin.dart';
 import 'dart:html' show HttpRequest;
 import 'dart:js' as js;
+
+bool blackCheck = true;
 
 void main() {
   runApp(MyApp());
@@ -15,21 +17,36 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: _title,
-      theme: ThemeData(
-        brightness: Brightness.light,
-        primarySwatch: Colors.blue,
-      ),
-      darkTheme:
-      ThemeData(brightness: Brightness.dark, primarySwatch: Colors.blue),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => FirstPage(),
-        '/second': (context) => SecondPage()
-      },
-    );
-  }
+
+      return MaterialApp(
+        title: _title,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          primarySwatch: Colors.blue,
+        ),
+        darkTheme:
+        ThemeData(brightness: Brightness.dark, primarySwatch: Colors.blue),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => FirstPage(),
+          '/second': (context) => SecondPage()
+        },
+      );
+    }
+    /*
+    else {
+      return MaterialApp(
+        title: _title,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => FirstPage(),
+          '/second': (context) => SecondPage()
+        },
+      );*/
+   // }
 }
 
 class FirstPage extends StatefulWidget {
@@ -44,9 +61,11 @@ class _FirstPage extends State<FirstPage> {
     coinList.forEach(() => {}) : qoduf dksdml zmffotm rkrrkrdp eogo gkatnfmf wnf tndlTek.
     얘를 이ㅛㅇㅇ해 어떻게 리턴을 정의할 것인가? {} 안에 row 넣고, 그 child란에 text 넣는다 가정시, text()
      */
+
   List<Coin> coinList = [Coin("a","b","c","d","e"),
     Coin("e","f","g","h","i"), Coin("j","k","l","m","n"),
     Coin("o","p","q","r","s"), Coin("t","u","v","w","x")];
+
 
   ScrollController _scrollController = new ScrollController();
   int page = 1;
@@ -124,129 +143,221 @@ class _FirstPage extends State<FirstPage> {
     );
   }
 
+  Brightness bright = Brightness.dark;
+  Color textInSca = Colors.yellow;
   @override
   Widget build(BuildContext context) {
     //input in textefield
     TextEditingController varUrl = TextEditingController();
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('DYOR'),
+    return MaterialApp (
+      theme: ThemeData(
+        brightness: bright,
+        primarySwatch: Colors.blue,
       ),
-      body: Container(
-        child: Center(
-          child:Column (
-            children: <Widget> [
-              Padding(
-                padding: EdgeInsets.all(15),
-                child:Text('Text', style: TextStyle(fontSize: 40),),
-              ),
-              Padding(
-                padding: EdgeInsets.only(left : MediaQuery.of(context).size.width / 3 ,
-                    right :MediaQuery.of(context).size.width / 3),
-                child:TextField(controller: varUrl,
-                  decoration: InputDecoration(labelText: '치고싶은거쳐 ${coinList.length}',
-                  ),),
-              ),
-              Padding(
-                padding: EdgeInsets.all(15),
-                child:RaisedButton(
-                    child: Text('submit'),
-                    onPressed: () {
-                        AlertDialog dialog = new AlertDialog(
-                          title: Text('할 일'),
-                          content: Text('기타 뭐 받아서 할거'),
-                          actions: <Widget> [
-                            FlatButton(
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                              child: Text('x')
-                            ),
-                            ]
-                        );
-                        showDialog(
-                          context: context,
-                          builder: (BuildContext context) => dialog);
-                      },
-                    ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(right: MediaQuery.of(context).size.width / 3),
-                child:Center(
-                  child:Row(
-                    children:<Widget> [
-                      Padding(
-                        padding: EdgeInsets.only(left: MediaQuery.of(context).size.width / 3),
-                        child:Text('list', style: TextStyle(fontSize: 20, color: Colors.yellow,), ),
-                      ),
+    //  darkTheme:
+    //  ThemeData(brightness: Brightness.dark, primarySwatch: Colors.blue),
 
-                      /*
-                      버튼 3개중 뭐를 넣느냐에 따라 genre가 달라짐.
-                       */
-                      Padding(
-                        padding: EdgeInsets.only(left: 0, right: 1),
-                        child: MaterialButton(
-                          shape: CircleBorder(side: BorderSide(width: 2, color: Colors.red, style: BorderStyle.solid)),
-                          child: Text("P"),
-                          color: Colors.teal,
-                          textColor: Colors.amber,
-                          onPressed: (){
-                            setState( () {
-                              genre = 1;
-                            });},
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(left: 0, right: 1),
-                        child: MaterialButton(
-                        shape: CircleBorder(side: BorderSide(width: 2, color: Colors.red, style: BorderStyle.solid)),
-                        child: Text("G"),
-                        color: Colors.teal,
-                        textColor: Colors.amber,
-                        onPressed: (){ setState( () {
-                          genre = 2;
-                        }); },
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(left: 0, right: 1),
-                        child: MaterialButton(
-                          shape: CircleBorder(side: BorderSide(width: 2, color: Colors.red, style: BorderStyle.solid)),
-                          child: Text("B"),
-                          color: Colors.teal,
-                          textColor: Colors.amber,
-                          onPressed: (){ setState( () {
-                            genre = 3;
-                          }); },
-                        ),
-                      ),
-                    ]
-                  )
-                )
-              ),
-              Padding(
-                padding: EdgeInsets.all(15),
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.vertical,
-                  //child: SizedBox(
-                  child:SingleChildScrollView(
-                      child: _getDataTable(),
+        home: Scaffold(
+          appBar: AppBar(
+              title: Text('DYOR'),
+              actions: <Widget> [
+                Padding(
+                  padding: EdgeInsets.only(right: 20),
+                  child: GestureDetector(
+                      onTap: () {},
+                      child: Icon(
+                        Icons.more_vert,
+                      )
                   ),
                 ),
+              ]
+          ),
+          drawer: Drawer(
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: <Widget>[
+                  ListTile(
+                    leading: Icon(
+                      Icons.settings,
+                      color: Colors.white,
+                    ),
+                    title: Text('Setting'),
+                    onTap: () {
+                      AlertDialog dialog = new AlertDialog(
+                          title: Text('흑색모드'),
+                          actions: <Widget> [
+                            Switch(
+                                value: blackCheck,
+                                onChanged: (value) {
+                                  setState( () {
+                                    blackCheck = value;
+                                    if(blackCheck == true) {
+                                      bright = Brightness.dark;
+                                      textInSca = Colors.yellow;
+                                    }
+                                    else {
+                                      bright = Brightness.light;
+                                      textInSca = Colors.brown;
+                                    }
+                                  });
+                                }
+                            )
+                          ]
+                      );
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) => dialog);
+                    },
+                    trailing: Icon(Icons.more_vert_rounded),
+                  ),
+                ],
               )
-            ]
-          )
-        ) ,
-        ),
+          ),
 
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.of(context).pushNamed('/second');
-        },
-        child: Icon(Icons.add),
-      ),
+          body: Container(
+            child: Center(
+                child:Column (
+                    children: <Widget> [
+                      Padding(
+                        padding: EdgeInsets.all(15),
+                        child:Text('Text', style: TextStyle(fontSize: 40),),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left : MediaQuery.of(context).size.width / 3 ,
+                            right :MediaQuery.of(context).size.width / 3),
+                        child:TextField(controller: varUrl,
+                          decoration: InputDecoration(labelText: '치고싶은거쳐',
+                          ),),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(15),
+                        child:RaisedButton(
+                          child: Text('submit'),
+                          onPressed: () {
+                            AlertDialog dialog = new AlertDialog(
+                                title: Text('할 일'),
+                                content: Text('기타 뭐 받아서 할거'),
+                                actions: <Widget> [
+                                  FlatButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: Text('x')
+                                  ),
+                                ]
+                            );
+                            showDialog(
+                                context: context,
+                                builder: (BuildContext context) => dialog);
+                          },
+                        ),
+                      ),
+                      Padding(
+                          padding: EdgeInsets.only(right: MediaQuery.of(context).size.width / 3),
+                          child:Center(
+                              child:Row(
+                                  children:<Widget> [
+                                    Padding(
+                                      padding: EdgeInsets.only(left: MediaQuery.of(context).size.width / 4),
+                                      child:Text('list', style: TextStyle(fontSize: 20, color: textInSca,), ),
+                                    ),
+
+                                    /*
+                      버튼 3개중 뭐를 넣느냐에 따라 genre가 달라짐.
+                       */
+                                    Padding(
+                                      padding: EdgeInsets.only(left: MediaQuery.of(context).size.width / 100),
+                                      child: MaterialButton(
+                                        shape: CircleBorder(side: BorderSide(width: 2, color: Colors.red, style: BorderStyle.solid)),
+                                        child: Text("P"),
+                                        color: Colors.teal,
+                                        textColor: Colors.amber,
+                                        onPressed: (){
+                                          setState( () {
+                                            genre = 1;
+                                          });},
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.only(left: MediaQuery.of(context).size.width / 100),
+                                      child: MaterialButton(
+                                        shape: CircleBorder(side: BorderSide(width: 2, color: Colors.red, style: BorderStyle.solid)),
+                                        child: Text("G"),
+                                        color: Colors.teal,
+                                        textColor: Colors.amber,
+                                        onPressed: (){ setState( () {
+                                          genre = 2;
+                                        }); },
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.only(left: MediaQuery.of(context).size.width / 100),
+                                      child: MaterialButton(
+                                        shape: CircleBorder(side: BorderSide(width: 2, color: Colors.red, style: BorderStyle.solid)),
+                                        child: Text("B"),
+                                        color: Colors.teal,
+                                        textColor: Colors.amber,
+                                        onPressed: (){ setState( () {
+                                          genre = 3;
+                                        }); },
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.only(left: MediaQuery.of(context).size.width / 100),
+                                      child: MaterialButton(
+                                        shape: CircleBorder(side: BorderSide(width: 2, color: Colors.lightBlueAccent, style: BorderStyle.solid)),
+                                        child: Text("?", style: TextStyle(color:Colors.brown)),
+                                        color: Colors.white,
+                                        textColor: Colors.amber,
+                                        onPressed: () {
+                                          AlertDialog dialog = new AlertDialog(
+                                              content: Text('P를 누르면 팬케이크스왑인지 뭐시기가 나오고\n'
+                                                  'G를 누르면 뭐가 나올지 나도 모르고\n'
+                                                  'B를 누르면 아무튼 뭐가 나오겠지 뭐'),
+                                              actions: <Widget> [
+                                                FlatButton(
+                                                    onPressed: () {
+                                                      Navigator.of(context).pop();
+                                                    },
+                                                    child: Text('x')
+                                                ),
+                                              ]
+                                          );
+                                          showDialog(
+                                              context: context,
+                                              builder: (BuildContext context) => dialog);
+                                        },
+                                      ),
+                                    ),
+                                  ]
+                              )
+                          )
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(15),
+                        child: SingleChildScrollView(
+                          scrollDirection: Axis.vertical,
+                          //child: SizedBox(
+                          child:SingleChildScrollView(
+                            child: _getDataTable(),
+                          ),
+                        ),
+                      )
+                    ]
+                )
+            ) ,
+          ),
+
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed('/second');
+            },
+            child: Icon(Icons.add),
+          ),
+        ),
     );
+
   }
 }
 
@@ -254,6 +365,31 @@ class _FirstPage extends State<FirstPage> {
 class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Container( //컨테이너로 감싼다.
+        decoration: BoxDecoration( //decoration 을 준다.
+            image: DecorationImage(
+                image: AssetImage("images/logo.png"), fit: BoxFit.cover)),
+        child: Scaffold(
+          backgroundColor: Colors.transparent, //스캐폴드에 백그라운드를 투명하게 한다.
+          appBar: AppBar(
+            elevation: 0,
+            backgroundColor: Colors.transparent,
+            title: Text('My App'),
+            centerTitle: true,
+            leading: IconButton(
+                icon: Icon(
+                  Icons.list,
+                  color: Colors.white,
+                ),
+                onPressed: () {}),
+          ),
+        ),
+      ),
+    );
+    /*
     return Scaffold(
       appBar: AppBar(
         title: Text('Second Page'),
@@ -268,6 +404,6 @@ class SecondPage extends StatelessWidget {
           ),
         ),
       ),
-    );
+    );*/
   }
 }
